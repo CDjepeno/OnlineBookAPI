ff495ca73ed22301c7cdea075ec2b33bb90c4aa5
robleh <hassan.alirobleh@gmail.com>
2023-07-24 14:26:22 +0200

    Config db with typeorm

--- src/infra/nest/src/app.module.ts
+++ src/infra/nest/src/app.module.ts
@@ -2,19 +2,38 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'Bonjour_12',
-      database: 'book_db',
-      entities: [],
-      synchronize: true,
-    }),
+    
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    })
+
+    // TypeOrmModule.forRoot({ 
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'Bonjour_12',
+    //   database: 'book_db',
+    //   entities: [],
+    //   synchronize: true,
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],

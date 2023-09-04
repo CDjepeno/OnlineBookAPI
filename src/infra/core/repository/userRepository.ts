81b3f00a7600f2d824dffe03e2d526f000f220e0
robleh <hassan.alirobleh@gmail.com>
2023-09-04 20:09:39 +0200

    supprission dossier core dans l'infra

--- src/infra/core/repository/userRepository.ts
+++
@@ -1,9 +0,0 @@
-import { ValidationError } from "@nestjs/common";
-import { UserDTO } from "../dtos/user.dtos";
-
-
-export interface IUserRepository {
-
-    createUser(user: UserDTO): Promise<string | ValidationError[]>
-}
-

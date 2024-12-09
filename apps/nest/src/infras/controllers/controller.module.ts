e9438e4b2df732650a167e49cc9d0dc4e9c46b46
Cdjepeno <djepenodev@gmail.com>
2024-12-10 00:08:11 +0100

    :sparkles: POC-ONELINEBOOK update

--- apps/nest/src/infras/controllers/controller.module.ts
+++ apps/nest/src/infras/controllers/controller.module.ts
@@ -5,17 +5,6 @@ import multer from 'multer';
 import { JwtAuthGuard } from 'src/infras/common/guards/jwt-auth.guard';
 import { AwsS3Client } from '../clients/aws/aws-s3.client';
 import { UsecaseProxyModule } from '../usecase-proxy/usecase-proxy.module';
-import { AddBookController } from './book/addBook/addBook.controller';
-import { DeleteBookController } from './book/deleteBook/deleteBook.controller';
-import { GetAllBookController } from './book/getAllBook/getAllBook.controller';
-import { GetBookController } from './book/getBook/getBook.controller';
-import { GetBookByUserController } from './book/getBookByUser/getBookByUser.controller';
-import { UpdateBookController } from './book/updateBook/updateBook.controller';
-import { AuthController } from './user/auth.controller';
-import { UsersController } from './user/users.controller';
-import { BookingBookController } from './booking/bookingBook/bookingBook.controller';
-import { GetBookingBookController } from './booking/getBookingBook/getbookingBook.controller';
-import { GetBookByNameController } from './book/getBooByName/getBookByName.controller';
 import * as path from 'path';
 import * as glob from 'glob';
 
@@ -24,9 +13,6 @@ function loadControllers(): any[] {
   const files = glob.sync(
     path.join(__dirname, './**/*.controller.{ts,js}'),
   );
-  console.log('files---------------------------------------------------');
-  console.log(files);
-  console.log(__dirname);
   
   for (const file of files) {
     const module = require(file);
@@ -50,7 +36,3 @@ loadControllers()
   providers: [JwtAuthGuard, JwtService, AwsS3Client],
 })
 export class ControllerModule {}
-
-// onlineBookApi/apps/nest/src/infras/controllers/book
-// onlineBookApi/apps/nest/src/infras/controllers/booking
-// onlineBookApi/apps/nest/src/infras/controllers/user
